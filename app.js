72454ef0bfe3e12929c11a56ede22ed95b8f16e3
Aleksey Shemyakin <carl0slit0s@yndex.ru>
2022-06-21 20:59:34 +0500

    add use not found path

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.use((req, res, next) => {
 app.use("/users", require("./routes/users"));
 app.use("/cards", require("./routes/cards"));
 
+app.use((req,res) => {
+  res.status(404).send({message: 'страницы не существует'})
+})
+
 app.listen(PORT, () => {
   console.log("123");
 });
