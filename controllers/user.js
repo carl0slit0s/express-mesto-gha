ee17a16d1d219b90e09f24c7a3003b700e61fc21
Aleksey Shemyakin <carl0slit0s@yndex.ru>
2022-07-10 01:46:16 +0500

    for tests

--- controllers/user.js
+++ controllers/user.js
@@ -132,12 +132,13 @@ module.exports.login = (req, res, next) => {
         if (!isPasswordCorrect) {
           authErorr();
         }
-        return jwt.sign({ id: user._id }, 'very_secret');
+        const token = jwt.sign({ id: user._id }, 'very_secret');
+        res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true });
         // { maxAge: 3600000 * 24 * 7 }
       })
-      .then((token) => {
-        res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true });
-      })
+      // .then((token) => {
+      //   res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true });
+      // })
       .catch(next);
   } catch (err) {
     next(err);
