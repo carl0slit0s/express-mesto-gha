a77680fc4b1228c5212ed7b308708ae53a9473ee
Aleksey Shemyakin <carl0slit0s@yndex.ru>
2022-07-10 01:05:05 +0500

    for tests

--- routes/cards.js
+++ routes/cards.js
@@ -12,7 +12,7 @@ const {
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().pattern(/(http|https):\/\/([\w.]+\/?)\S*/),
+    link: Joi.string().required().pattern(/(http|https):\/\/([\w.]+\/?)\S*/),
   }),
 }), addCard);
 
