f35266a5ced7d1b9921f80dcd4b3698ab6af6de7
Aleksey Shemyakin <carl0slit0s@yndex.ru>
2022-07-09 21:27:36 +0500

    for tests

--- routes/cards.js
+++ routes/cards.js
@@ -18,9 +18,21 @@ router.post('/', celebrate({
 
 router.get('/', getCards);
 
-router.delete('/:cardId', deleteCard);
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), deleteCard);
 
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), likeCard);
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), dislikeCard);
 
 module.exports = router;
